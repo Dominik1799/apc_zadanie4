ae9625eedbee3f82d73bd7f35c4da1fe6caf70b2
Dominik1799 <dom.horvath17@gmail.com>
2020-10-20 20:37:01 +0200

    bruh

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#
+#include <fstream>
 
 struct Metadata {
     std::string inputFile;
     std::string outputFile;
-    std::vector<size_t> columns;
+    std::vector<size_t> columnNumbers;
     std::vector<std::vector<std::string>> table;
 };
 
@@ -16,10 +16,9 @@ void exitProgram(int exitCode, const std::string& message){
     exit(exitCode);
 }
 
-std::vector<size_t> parseColumnNumbers(std::string& rawColumns){
-    std::vector<size_t> columns;
+void parseColumnNumbers(std::string& rawColumns,std::vector<size_t>& columns){
     bool wasNumber = false;
-    size_t begin = 0;
+    size_t num,begin = 0;
     for(size_t i = 0; i < rawColumns.size(); i++){
         if ((rawColumns[i] > '0' && rawColumns[i] <= '9') || (rawColumns[i] == '0' && wasNumber)){
             begin = wasNumber ? begin : i;
@@ -28,15 +27,27 @@ std::vector<size_t> parseColumnNumbers(std::string& rawColumns){
             if (!wasNumber || i == rawColumns.size()-1)
                 exitProgram(1,"3rd argument has invalid format.");
             std::string subst = rawColumns.substr(begin,i-begin);
-            columns.push_back(std::stoll(subst));
+            num = std::stoll(subst);
+            if (std::find(columns.begin(),columns.end(),num) != columns.end()){
+                exitProgram(1,"3rd argument has invalid format: Duplicates.");
+            }
+            columns.push_back(num);
             wasNumber = false;
         } else {
             exitProgram(1, "3rd argument has invalid format.");
         }
     }
     std::string subst = rawColumns.substr(begin);
-    columns.push_back(std::stoll(subst));
-    return columns;
+    num = std::stoll(subst);
+    if (std::find(columns.begin(),columns.end(),num) != columns.end()){
+        exitProgram(1,"3rd argument has invalid format: Duplicates.");
+    }
+    columns.push_back(num);
+}
+
+std::vector<std::string> parseRow(std::string& row){
+    std::vector<std::string> result;
+    return result;
 }
 
 void readData(int argc, char* argv[]){
@@ -48,7 +59,12 @@ void readData(int argc, char* argv[]){
     Metadata metadata;
     metadata.inputFile = argv[1];
     metadata.outputFile = argv[2];
-    metadata.columns = parseColumnNumbers(str);
+    parseColumnNumbers(str,metadata.columnNumbers);
+    std::ifstream input(metadata.inputFile);
+    std::string buffer;
+    while (std::getline(input,buffer)){
+        std::cout << buffer;
+    }
 
 }
 
